45e6aaf3d1752ebd8bace60d2b4df19ae0f30997
khaled-alahmad <eng.khaledalahmad@gmail.com>
2025-03-17 22:09:26 +0300

    `Added authentication route redirects and updated protected routes`

--- middleware.js
+++ middleware.js
@@ -4,13 +4,18 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
     const token = cookies().get("token")?.value;
 
-
     const protectedRoutes = ["/", "/games", "/users", "/content", "/notifications", "/sugar", "/appointments"];
+    const authRoutes = ["/login", "/signup","/"]; // المسارات التي لا يجب أن يراها المستخدم إذا كان مسجل دخول
 
     if (protectedRoutes.some((route) => request.nextUrl.pathname.startsWith(route))) {
         if (!token) {
             return NextResponse.redirect(new URL("/login", request.url));
         }
+    }
+
+    // إذا كان المستخدم مسجلاً الدخول وحاول الوصول إلى صفحات تسجيل الدخول، نوجهه إلى /dashboard
+    if (authRoutes.includes(request.nextUrl.pathname) && token) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
     return NextResponse.next();
@@ -20,10 +25,7 @@ export const config = {
     matcher: [
         "/",
         "/games/:path*", "/users/:path*", "/content/:path*", "/notifications/:path*", "/sugar/:path*", "/appointments/:path*",
-
         "/settings/:path*",
-
-
-
+        "/login", "/signup" // نضيف صفحات المصادقة أيضاً إلى `matcher`
     ],
 };
