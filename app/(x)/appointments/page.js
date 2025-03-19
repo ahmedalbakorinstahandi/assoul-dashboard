eeee93d84e39ec4ce40d9094fc910b49d034a840
khaled-alahmad <eng.khaledalahmad@gmail.com>
2025-03-19 05:25:54 +0300

    `Dynamic import of AppointmentsManagement component and disabling SSR`

--- app/(x)/appointments/page.js
+++ app/(x)/appointments/page.js
@@ -1,4 +1,9 @@
-import { AppointmentsManagement } from "@/components/appointments-management";
+import dynamic from "next/dynamic";
+
+// استيراد مكون AppointmentsManagement ديناميكيًا
+const AppointmentsManagement = dynamic(() => import('@/components/appointments-management'), {
+  ssr: false // تعطيل التوليد المسبق (SSR)
+});
 
 export default function Page() {
   return <AppointmentsManagement />;
